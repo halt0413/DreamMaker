b17f3271fce626609d9899b576c862183ac3271d
hal <halt0413@gmail.com>
2025-08-02 16:43:23 +0900

    映画画面を追加し、ナビゲーションスタックの順序を更新

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import HomeScreen from './features/home/HomeScreen'
 import GalleryScreen from './features/gallery/GalleryScreen'
 import { RootStackParamList } from './navigation/types'
 import { NavigationContainer } from '@react-navigation/native'
+import  MovieScreen  from './features/movie/MovieScreen'
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
 
@@ -12,7 +13,8 @@ export default function AppNavigator() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Gallery" component={GalleryScreen} />
+        <Stack.Screen name="Movie" component={MovieScreen} />
+        <Stack.Screen name="Gallery" component={GalleryScreen} /> 
       </Stack.Navigator>
     </NavigationContainer>
   )
